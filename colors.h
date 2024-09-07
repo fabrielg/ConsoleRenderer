a0aac52edfe44b3558f64f28af2472782e15edb3
fabrielg <gfrancois2003@gmail.com>
2024-09-07 22:04:11 +0200

    feat: add colors ansi value in the header colors.h

---
+++ colors.h
@@ -0,0 +1,28 @@
+#ifndef COLOR_H
+# define COLOR_H
+
+# define reset 0
+
+# define black 30
+# define red 31
+# define green 32
+# define yellow 33
+# define blue 34
+# define magenta 35
+# define cyan 36
+# define white 37
+# define default 39
+
+# define black_b 40
+# define red_b 41
+# define green_b 42
+# define yellow_b 43
+# define blue_b 44
+# define magenta_b 45
+# define cyan_b 46
+# define white_b 47
+# define default_b 49
+
+void    write_color(char *str, int color);
+
+#endif
