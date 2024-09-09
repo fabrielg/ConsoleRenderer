2c77436f8666bbbcfd784cdda47cf4db5096eaf1
fabrielg <gfrancois2003@gmail.com>
2024-09-09 19:34:15 +0200

    test: init buffer by the window size

--- main.c
+++ main.c
@@ -1,16 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "colors.h"
+#include "window.h"
 
-void    write_color(char *str, int color)
+void write_color(char *str, int color)
 {
-    printf("%c[%dm%s\n", 27, color, str);
+	printf("\e[%dm%s\n", color, str);
 }
 
-int main(int ac, char **av)
+typedef struct s_position
 {
-    if (ac == 2)
-    {
-        write_color(av[1], magenta);
-    }
-    return (0);
+	int	x;
+	int	y;
+}	t_position;
+
+int	position_to_index(t_position pos, int width)
+{
+	return (pos.y * (width + 1) + pos.x);
+}
+
+void	write_in_buffer(char c, char *buffer, int size, int position)
+{
+	if (position < size)
+		buffer[position] = c;
+}
+
+void	clear_buffer(char *buffer, int size)
+{
+	int	i;
+
+	i = -1;
+	while (++i < size)
+		buffer[i] = '\0';
+}
+
+char	*init_buffer(int size)
+{
+	char *buffer;
+
+	buffer = malloc(size);
+	if (!buffer)
+		return (NULL);
+	clear_buffer(buffer, size);
+	return (buffer);
+}
+
+void init_window(t_window *window, int width, int height)
+{
+	int	size;
+
+	if (width < 0)
+		width = 0;
+	if (height < 0)
+		height = 0;
+	window->width = width;
+	window->height = height;
+	size = sizeof(char) * ((width + 1) * height);
+	window->buffer.size = size;
+	window->buffer.string = init_buffer(size + sizeof(char));
+}
+
+int	main(int ac, char **av)
+{
+	t_window window;
+
+	if (ac == 2 || 1)
+	{
+		init_window(&window, 64, 32);
+		if (window.buffer.string)
+		{
+			free(window.buffer.string);
+		}
+	}
+	return (0);
 }
